2abc1ba9fad4029c0225df9308c8338b37b8f4d5
Mac Mini - Dương Khang Hy (Eric Duong) <ericduong107@gmail.com>
2024-04-18 00:04:49 +0700

    Change and complete w3_band

--- src/w3_band/js/main.js
+++ src/w3_band/js/main.js
@@ -74,35 +74,3 @@ modal.addEventListener("click", hideBuyTickets);
 modalContainer.addEventListener("click", function (event) {
   event.stopPropagation(); // Ngừng nổ bọt
 });
-
-// ---------------------------------
-/*
-// Đường dẫn đến API bạn muốn lấy dữ liệu
-const apiUrl = "https://ericduong107.github.io/api/home/projects.json";
-
-function getData() {
-  // Sử dụng fetch để gửi yêu cầu GET đến API
-  fetch(apiUrl)
-    .then((response) => {
-      // Kiểm tra xem có lỗi không
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      // Nếu không có lỗi, giải mã dữ liệu JSON
-      return response.json();
-    })
-    .then((data) => {
-      // Xử lý dữ liệu ở đây, ví dụ: log ra console
-      console.log(data);
-      return data;
-    })
-    .catch((error) => {
-      // Xử lý các lỗi xảy ra trong quá trình lấy dữ liệu
-      console.error("There was a problem with the fetch operation:", error);
-    });
-}
-
-var data = getData();
-
-var isCheck = alert(data.toString());
-*/
